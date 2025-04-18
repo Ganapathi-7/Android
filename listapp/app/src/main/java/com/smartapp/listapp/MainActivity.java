20fb29872decc74268007549246b7d231230648f
GanapathiGummala <ganapathi:@kanilebettu.com>
2025-04-19 00:57:10 +0530

    life cycle methods done

--- listapp/app/src/main/java/com/smartapp/listapp/MainActivity.java
+++ listapp/app/src/main/java/com/smartapp/listapp/MainActivity.java
@@ -1,77 +1,87 @@
+package com.smartapp.listapp;
+
+import static android.content.ContentValues.TAG;
+
+import android.database.DatabaseUtils;
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import com.smartapp.listapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-    private ActivityMainBinding activityMainBinding;
+    private ActivityMainBinding activityMainBinding;  // Declare the binding variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
-
-        // Logging onCreate
-        Log.d(TAG, "onCreate() called");
-
-        // Your code for initialization goes here
+        activityMainBinding.textView.setText("Hello Ganapathi!");
     }
+
 
     @Override
     protected void onStart() {
         super.onStart();
         // Logging onStart
-        Log.d(TAG, "onStart() called");
+        Log.d("TAG", "onStart() called");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         // Logging onResume
-        Log.d(TAG, "onResume() called");
+        Log.d("TAG", "onResume() called");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         // Logging onPause
-        Log.d(TAG, "onPause() called");
+        Log.d("TAG", "onPause() called");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         // Logging onStop
-        Log.d(TAG, "onStop() called");
+        Log.d("TAG", "onStop() called");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         // Logging onRestart
-        Log.d(TAG, "onRestart() called");
+        Log.d("TAG", "onRestart() called");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // Logging onDestroy
-        Log.d(TAG, "onDestroy() called");
+        Log.d("TAG", "onDestroy() called");
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         // Saving state before the activity is destroyed
-        Log.d(TAG, "onSaveInstanceState() called");
+        Log.d("TAG", "onSaveInstanceState() called");
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         // Restoring state after activity is recreated
-        Log.d(TAG, "onRestoreInstanceState() called");
+        Log.d("TAG", "onRestoreInstanceState() called");
     }
+
+
 }
